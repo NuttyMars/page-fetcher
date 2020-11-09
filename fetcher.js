a43001af18b7be9feac2ef47606b584fb926d1df
Natalia Martian <natalia.martian@gmail.com>
2020-11-09 20:56:47 +0000

    Refactored the code to do less checks

--- fetcher.js
+++ fetcher.js
@@ -1,41 +1,47 @@
 const request = require('request');
 const fs = require('fs');
 
+//extract command line arguments
 const args = process.argv.slice(2);
-const domain = args[0];
+const URL = args[0];
 const localPath = args[1];
 
-request(`${domain}`, (err, body) => {
-
-  fs.stat('./index.html', function(err, stat) {
-
-    // file exists
-    if (err == null) {
-      // if file exists we should make sure it is an actual file we can write
-      if (!stat.isFile) {
-        console.log('Cannot write to this type of file!');
-      } else {
-        // file exists already and it is a valid type
-        console.log('File exists, overwiting!');
-        fs.writeFile('./index.html', body, function (err) {
-          if (err) throw err;
-        });
-      }
-    } else if (err.code === 'ENOENT') {
-      // file does not exist
-      fs.writeFile('./index.html', body, function (err) {
-        if (err) throw err;
-      });
-    }
-    let fileSize = stat.size;
-
-    console.log(`Downloaded and saved ${fileSize} bytes to ${localPath}`);
-
+const dataRequest = function(URL, localPath) {
+  request(URL, (error, response, body) => {
+  
+    //if an error happens during the request
+    if(error) {
+      console.log(`Failed to download. Error details: ${error}`);
+      return;
+    };
+  
+    //if there's not an error but the status code is not ok
+    const isStatusCodeNotOK = response.statusCode !== 200;
+  
+    if (isStatusCodeNotOK) {
+      console.log(`Something went wrong! Status message: ${response.statusMessage}\nTerminating connection...`)
+      return;
+    };
+  
+    //this overwrites the local file
+    fs.writeFile(localPath, body, function(err) {
+  
+      //throw error if there is one
+      if (err) throw err;
+    
+      //overwrite the content
+      //body.length => counts the length of the string that is returned with the body
+      //each letter is approximately a byte
+      console.log(`Downloaded and saved ${body.length} bytes to ${localPath}`);
+    });
+  
   });
-});
+}
 
-//let stats = fs.statSync('./index.html');
-//let fileSize = stats['size'];
+//if the URL or local path do not get passed in the terminal
+if (!URL || !localPath) {
+  console.log('Please use the following format for your request: node fetcher.js <url> <localpath>');
 
-// console.log(`Downloaded and saved ${fileSize} bytes to ${localPath}`);
-
+} else { 
+  dataRequest(URL, localPath);
+};
